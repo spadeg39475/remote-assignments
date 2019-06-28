1a0807851cfcb4a0d7d957bf21f9c3e4455fa0b5
Josh Yang <spadeg39475@gmail.com>
2019-06-28 16:58:48 +0800

    week-3 assignment

---
+++ week-3/Node.js/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/getData', (req, res) => {
+    
+    var r = /^[0-9]*[1-9][0-9]*$/ //正整數 
+    var sum = 0;
+    const { number } = req.query;
+
+    if(number){
+        if(r.test(number)){
+            for(var i=1; i <= number; i++){
+                sum += i;
+            }
+            res.send(sum.toString());
+
+        }else{
+            res.send('Wrong Parameter');
+        }
+    }
+    res.send('Lack of Parameter');
+});
+
+
+app.post('/sum.html', (req, res) => {
+    res.render('sum', { name: req.body.number });
+});
+
+
+app.get('/myName', (req, res) => {
+    const userName = req.query.name;
+    
+    if(userName){
+        res.cookie('userName', userName);
+        res.render('myName', {name: userName});
+    }else{
+      const name = req.cookies.userName;
+      res.render('myName', {name: name});
+    }
+});
+
+app.post('/myName', (req, res) => {
+    res.cookie('userName', req.body.userName);
+    res.redirect('/myName');
+});
+
+app.post('/logOut', (req,res) =>{
+    res.clearCookie('userName');
+    res.redirect('/myName');
+});
+
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}!`);
+});
